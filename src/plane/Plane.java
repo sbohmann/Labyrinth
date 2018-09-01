47af3fadf414781cbedce1252cae097ffaaad609
Sebastian Bohmann <mail@sebastian-bohmann.com>
2018-09-01 13:22:33 +0200

    Removed chunk initialization on get

--- src/plane/Plane.java
+++ src/plane/Plane.java
@@ -12,18 +12,26 @@ public class Plane<E> {
 
     @SuppressWarnings("unchecked")
     public E get(int x, int y) {
-        Object[] chunk = getChunk(x, y);
+        Object[] chunk = getChunkOrNull(x, y);
+        if (chunk == null) {
+            return null;
+        }
         int offset = ChunkAddress.chunkOffset(x, y);
         return (E) chunk[offset];
     }
 
     public void set(int x, int y, E value) {
-        Object[] chunk = getChunk(x, y);
+        Object[] chunk = getOrCreateChunk(x, y);
         int offset = ChunkAddress.chunkOffset(x, y);
         chunk[offset] = value;
     }
 
-    private Object[] getChunk(int x, int y) {
+    private Object[] getChunkOrNull(int x, int y) {
+        Long key = ChunkAddress.chunkKey(x, y);
+        return chunks.get(key);
+    }
+
+    private Object[] getOrCreateChunk(int x, int y) {
         Long key = ChunkAddress.chunkKey(x, y);
         return chunks.computeIfAbsent(key, key_ -> new Object[ChunkAddress.ChunkSize]);
     }
